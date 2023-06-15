30a58be1c3dac301b160cc9722007a2fc6f2eaec
paugrau <52864303+paugrau@users.noreply.github.com>
2023-06-15 14:01:23 +0200

    Created RDD_pau
    Very preliminary work

---
+++ replication/rdd_pau.R
@@ -0,0 +1,38 @@
+
+library(rdrobust)
+library(patchwork)
+# 0. Import data
+
+source2('code/data.R', 63,288) # this does the data prep for us. 
+
+# 1. Clean data
+
+df <- df %>%
+  mutate(days_=as.numeric(days))
+
+# 2. Descriptives
+
+df%>%ggplot(aes(x=days_, y=ext_eff, col=factor(treatment)))+geom_jitter()+geom_smooth(col="black")+geom_vline(xintercept=0)
+
+# RD:
+female    <-   rdrobust::rdplot(x=df$days_, y=df$female)$rdplot+labs(title = "Female")
+in_school <-   rdrobust::rdplot(x=df$days_, y=df$in_school)$rdplot+labs(title = "in_school")
+education <-   rdrobust::rdplot(x=df$days_, y=df$education)$rdplot+labs(title = "education")
+abitur    <-   rdrobust::rdplot(x=df$days_, y=df$abitur)$rdplot+labs(title = "abitur")
+kreisfrei <-   rdrobust::rdplot(x=df$days_, y=df$kreisfrei)$rdplot+labs(title = "kreisfrei")
+turnout   <-   rdrobust::rdplot(x=df$days_, y=df$turnout)$rdplot+labs(title = "turnout")
+
+patchwork::wrap_plots(
+  female,
+  in_school,
+  education,
+  abitur,
+  kreisfrei,
+  turnout
+  )
+
+## Variables of interest
+rdrobust::rdplot(x=df$days_, y=df$polint)
+rdrobust::rdplot(x=df$days_, y=df$ext_eff)
+rdrobust::rdplot(x=df$days_, y=df$int_eff)
+rdrobust::rdplot(x=df$days_, y=df$demsat)
