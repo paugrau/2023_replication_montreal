60ab8b29084c38b4aa6e92ed5212ef2f5895aa6c
paugrau <52864303+paugrau@users.noreply.github.com>
2023-07-08 12:25:10 +0200

    RDD

--- replication/rdd_pau.R
+++ replication/rdd_pau.R
@@ -31,6 +31,12 @@ patchwork::wrap_plots(
   turnout
   )
 
+rm(  female,
+     in_school,
+     education,
+     abitur,
+     kreisfrei,
+     turnout)
 ## Variables of interest
 rdrobust::rdplot(x=df$days_, y=df$polint)
 rdrobust::rdplot(x=df$days_, y=df$ext_eff)
@@ -49,4 +55,19 @@ patchwork::wrap_plots(
   int_eff,
   demsat)
 
+rm(polint, ext_eff, int_eff, demsat)
+
 df%>%filter(days_<100&days>-100)%>%ggplot(aes(x=days_, fill=factor(treatment)))+geom_histogram(bins=500)
+
+# OPTIMAL BANDWIDTH
+
+df_nona <- df%>%
+  ungroup()%>%
+  drop_na(days_, ext_eff, in_school, education, abitur, female, at_home, migration)%>%
+  select(days_, ext_eff, in_school, education, abitur, female, at_home, migration)
+rm(ext_eff)
+attach(df_nona)
+
+summary(rdrobust::rdrobust(y=ext_eff, x=days_, vce="nn", bwselect="msetwo", covs=cbind(in_school, education, abitur, female, at_home, migration)))
+summary(rdrobust::rdrobust(y=int_eff, x=days_, vce="nn", bwselect="msetwo", covs=cbind(in_school, education, abitur, female, at_home, migration)))
+summary(rdrobust::rdrobust(y=polint, x=days_, vce="nn", bwselect="msetwo", covs=cbind(in_school, education, abitur, female, at_home, migration)))
